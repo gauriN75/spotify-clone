b223a0170082fafd7e4ecec6745e6b6385650717
Gauri Nalawade <gaurinalawade84@gmail.com>
2023-02-18 11:27:34 +0530

    token

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 import Login from './Login';
 import { useEffect, useState } from 'react';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token,setToken] = useState();
+  const [token,setToken] = useState(null);
 
   // Run code based on a given condition
   useEffect(() => {
@@ -15,16 +19,22 @@ function App() {
 
       if(_token){
         setToken(_token)
+
+        spotify.setAccessToken(_token);
+
+        spotify.getMe().then((user) => {
+          console.log("user : ", user);
+        });
       }
 
-      console.log("I have a token : ", token);
-  },[]);
+      // console.log("I have a token : ", token);
+  }, []);
 
   return (
     <div className="App">
     {
       token ? (
-        <h1>Player: I'm loggoed in</h1>
+        <Player/>
       ) : (
         <Login/>
       )
